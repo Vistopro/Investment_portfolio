7be2da37c7e23938cb787de6fa1f96edb604bfb2
Vistopro <victor93martin@gmail.com>
2025-01-08 19:20:00 +0100

    IP-2: add edit user view

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -12,15 +12,18 @@ from portfolio.views import (
     SearchFinancialInstrumentView,
     HomeView,
     LoginView,
+    EditView,
     LogoutView
 )
 from django.conf.urls.static import static
 from django.conf import settings
 
+
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('login/', LoginView.as_view(), name='login'),
     path('register/', RegisterView.as_view(), name='register'),
+    path('edit/', EditView.as_view(), name='edit'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('portfolio/', PortfolioListView.as_view(), name='portfolio_list'),
     path('portfolio/add/', PortfolioCreateView.as_view(), name='portfolio_add'),
